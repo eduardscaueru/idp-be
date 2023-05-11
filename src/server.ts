fa0d7f4a1f6e0c90425f61602fd5242b1a755a89
eduardscaueru <edi_scaueru@yahoo.com>
2023-05-11 19:24:00 +0300

    Added cors url

--- src/server.ts
+++ src/server.ts
@@ -15,10 +15,9 @@ const main = async () => {
 
   //middlewares
   app.use(helmet());
-  // app.use(cors({
-  //   origin: ['http://localhost:4200']
-  // }))
-  app.use(cors());
+  app.use(cors({
+    origin: ['http://idp-fe:4200']
+  }));
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(upload());
 
